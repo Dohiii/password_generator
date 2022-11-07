29ff350132f73f5a4491c0c751949b4e032ef902
Dohiii <val.pcclinic@gmail.com>
2022-11-07 14:14:00 +0100

    final commit all is working

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ const copyBtn = document.getElementById("copy");
 const password = document.getElementById("password");
 const slider = document.getElementById("my_range");
 const output = document.getElementById("lenght_value");
-
 const how_strong_password = document.getElementById("how_strong_password");
-
 const password_image_svg = document.getElementById("password_image_svg");
 const popup = document.getElementsByClassName("popup");
 const abc_lower = document.getElementById("abc_lower");
-
 // selectors chackboxes
+const checkboxes = document.getElementsByClassName("checkboxes");
 const abc_upper_checkbox = document.getElementById("abc_upper_checkbox");
 
 const abcUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -21,30 +19,23 @@ const abcLower = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const specialCharacters = "!@#$%^&*";
 
-let passwordOptions = {
-  abcUpper: abcUpper,
-  abcLower: abcLower,
+const passwordOptions = {
+  abc_upper: abcUpper,
+  abc_lower: abcLower,
   numbers: numbers,
-  specialCharacters: specialCharacters,
+  special_chars: specialCharacters,
 };
 
-console.log(passwordOptions);
-
-const updateObject = () => {
-  if (passwordOptions.abcUpper === abcUpper) {
-    passwordOptions.abcUpper = "";
-  } else {
-    passwordOptions.abcUpper = abcUpper;
+function getValueCheckbox() {
+  const checkboxes = document.getElementsByName("checkbox_char");
+  let result = "";
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      result += passwordOptions[checkboxes[i].value];
+    }
   }
-};
-
-abc_upper_checkbox.addEventListener("change", (e) => {
-  if (e.target.checked) {
-    passwordOptions.abcUpper = "";
-  } else {
-    passwordOptions.abcUpper = abcUpper;
-  }
-});
+  return result;
+}
 
 // Slider
 how_strong_password.innerText = "strong";
@@ -65,7 +56,7 @@ refreshBtn.addEventListener("click", async () => {
   // console.log("Before the delay");
   reload_svg.classList.add("fa-spin");
 
-  await generatePassword(Number(output.innerText), allChars);
+  await generatePassword(Number(output.innerText));
 
   await delay(1);
 
@@ -84,11 +75,10 @@ copyBtn.addEventListener("click", copy);
 
 // Random password generator
 
-const allChars = Object.values(passwordOptions).join().replaceAll(",", "");
+// console.log(allChars);
 
-console.log(allChars);
-
-function generatePassword(length, characters) {
+function generatePassword(length) {
+  const characters = getValueCheckbox();
   let result = " ";
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
@@ -129,16 +119,3 @@ const popupFunc = async () => {
   await delay(2);
   popup.classList.toggle("show");
 };
-
-// popup.addEventListener("click", async () => {
-//   //Do what you want here
-//   // console.log("Before the delay");
-//   reload_svg.classList.add("fa-spin");
-
-//   await generatePassword(Number(output.innerText), allChars);
-
-//   await delay(1);
-
-//   // console.log("After the delay");
-//   reload_svg.classList.remove("fa-spin");
-// });
